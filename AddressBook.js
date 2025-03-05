7aa257e862e4a0b9c17c6b92bbcfbd88400b90f8
kuldeepsharma100 <149848065+kuldeepsharma100@users.noreply.github.com>
2025-03-05 20:00:45 +0530

    [kuldeep] add. Implemented search functionality by city or state

--- AddressBook.js
+++ AddressBook.js
@@ -77,4 +77,8 @@ class AddressBook {
         this.contacts.push(contact);
     }
 
+    searchByCityOrState(location) {
+        return this.contacts.filter(contact => contact.city === location || contact.state === location);
+    }
+
 }
