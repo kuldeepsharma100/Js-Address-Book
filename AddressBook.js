48d79c39244cf49078110ccf573f4d6426bb7352
kuldeepsharma100 <149848065+kuldeepsharma100@users.noreply.github.com>
2025-03-05 20:07:33 +0530

    [kuldeep] add. Used reduce() to get contact count by city/state

--- AddressBook.js
+++ AddressBook.js
@@ -91,4 +91,12 @@ class AddressBook {
         }, {});
     }
 
+    getCountByCityOrState() {
+        return this.contacts.reduce((result, contact) => {
+            result.cityCounts[contact.city] = (result.cityCounts[contact.city] || 0) + 1;
+            result.stateCounts[contact.state] = (result.stateCounts[contact.state] || 0) + 1;
+            return result;
+        }, { cityCounts: {}, stateCounts: {} });
+    }
+
 }
